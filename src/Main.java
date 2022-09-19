896feb132ab12e311f5524535b2e0374884e095d
Evgeniy Malygin <black2001@inbox.ru>
2022-09-19 15:56:10 +0300

    Выполнено задание по теме "Работа с исключениями"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
+
+    public static boolean checkRegisterData(String login, String password, String confirmPassword) {
+        try {
+            if (login.length() > 20) {
+                throw new WrongLoginExeption();
+            }
+            if (!password.equals(confirmPassword) || password.length() >= 20) {
+                throw new WrongPasswordExeption();
+            }
+        } catch (WrongLoginExeption e) {
+            System.out.println("Введен некорректный логин");
+            return false;
+        } catch (WrongPasswordExeption e) {
+            System.out.println("Введен некорректный пароль");
+            return false;
+        } finally {
+            System.out.println("Проверка данных завершена");
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String login = "LoginAaaaaBbbbbHjynm1"; //введен 21 символ
+        String password = "Aa12345678";
+        String confirmPassword = "Aa12345678";
+
+        System.out.println("checkRegisterData = " + checkRegisterData(login, password, confirmPassword));
+
     }
 }
